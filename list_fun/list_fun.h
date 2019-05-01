63602f6a7aaf254ae8174c1d50f344c900d0f379
softwareguycoder <softwareguycoder@gmail.com>
2019-05-01 08:31:03 -0400

    Added reference to test_core library and am starting test driver over

--- list_fun/list_fun.h
+++ list_fun/list_fun.h
@@ -6,18 +6,4 @@
 
 #include "list_fun_symbols.h"
 
-/**
- * @brief Forward declaration of POSITION structure
- */
-typedef struct _tagPOSITION POSITION;
-typedef struct _tagCLIENTSTRUCT CLIENTSTRUCT, *LPCLIENTSTRUCT;
-
-void AddToList(POSITION** ppListHead, void** ppvData);
-void AddToListHead(POSITION** ppNewListHead, void** ppvData);
-void DeleteFromTheMiddleTest();
-void HandleError(const char *pszMessage);
-void MakeElement(LPCLIENTSTRUCT *ppClientStruct);
-void PrintAllClientIDs(POSITION** ppHead);
-void PyramidAddTest();
-
 #endif //__LIST_FUN_H__
---
+++ list_fun/test_api.c
@@ -0,0 +1,8 @@
+/*
+ * test_api.c
+ *
+ *  Created on: May 1, 2019
+ *      Author: bhart
+ */
+
+
